609b9b1fdc07760ad64ebe61056621abcd23c291
Guilherme Murata <123330836+GuilhermeMurata@users.noreply.github.com>
2024-03-06 21:14:24 -0300

    resolução de problema, enviar link para tutor

---
+++ src/br/com/murata/Exercicio_mod9.java
@@ -0,0 +1,11 @@
+package br.com.murata;
+
+
+public class Exercicio_mod9 {
+    public static void main(String[] args) {
+
+        int valor = 5;
+        Integer wrapper = Integer.valueOf(valor);
+        System.out.println(wrapper);
+    }
+}
