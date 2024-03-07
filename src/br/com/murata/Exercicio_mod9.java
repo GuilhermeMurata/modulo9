2e0b1f554686ed29bdc56fcbdb76b8ab58423ebf
Guilherme Murata <123330836+GuilhermeMurata@users.noreply.github.com>
2024-03-07 12:21:49 -0300

    Fix: run da classe

--- src/br/com/murata/Exercicio_mod9.java
+++ src/br/com/murata/Exercicio_mod9.java
@@ -4,6 +4,13 @@ package br.com.murata;
 public class Exercicio_mod9 {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número inteiro: ");
+        int numeroPrimitivo = scanner.nextInt();
+
+        Integer numeroWrapper = Integer.valueOf(numeroPrimitivo);
+
         int valor = 5;
         Integer wrapper = Integer.valueOf(valor);
         System.out.println(wrapper);
